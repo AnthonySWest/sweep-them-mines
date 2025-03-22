c526172060bc7b347d5631d47d8384163ada50c4
Anthony West <anthony.s.west@gmail.com>
2025-03-22 13:49:05 -0700

    Add mine form icon and app style

--- Rad290/SweepThemMines.cpp
+++ Rad290/SweepThemMines.cpp
@@ -4,6 +4,8 @@
 #pragma hdrstop
 #include <tchar.h>
 //---------------------------------------------------------------------------
+#include <Vcl.Styles.hpp>
+#include <Vcl.Themes.hpp>
 USEFORM("..\Source\MainForm.cpp", FormMain);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
@@ -12,6 +14,7 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
     {
         Application->Initialize();
         Application->MainFormOnTaskBar = true;
+        TStyleManager::TrySetStyle("Cobalt XEMedia");
         Application->Title = "Sweep Them Mines";
         Application->CreateForm(__classid(TFormMain), &FormMain);
         Application->Run();
