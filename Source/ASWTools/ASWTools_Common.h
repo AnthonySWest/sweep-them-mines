601a3d5ab6c0bc46c205b2fb15b2c3917b3f2160
Anthony West <anthony.s.west@gmail.com>
2025-04-05 13:44:11 -0700

    Add ASWTools_Common

---
+++ Source/ASWTools/ASWTools_Common.h
@@ -0,0 +1,43 @@
+/* **************************************************************************
+ASWTools_Common.h
+Author: Anthony S. West - ASW Software
+
+Contains functions for creating a console window from a WinMain app.
+
+Every attempt should be made to keep this module at least Windows portable.
+
+Copyright 2025 Anthony S. West
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+************************************************************************** */
+
+//---------------------------------------------------------------------------
+#pragma once
+//---------------------------------------------------------------------------
+#ifndef ASWTools_CommonH
+#define ASWTools_CommonH
+//---------------------------------------------------------------------------
+
+#if __cplusplus < 201103L
+  #define nullptr NULL
+#endif
+
+//---------------------------------------------------------------------------
+
+namespace ASWTools
+{
+
+} // namespace ASWTools
+
+#endif // #ifndef ASWTools_CommonH
