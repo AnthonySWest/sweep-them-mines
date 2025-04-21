67e2dc3154678186c43a1059a6cd42460b154446
Anthony West <anthony.s.west@gmail.com>
2025-04-21 03:02:51 -0700

    Add click events to the engine

--- Source/ASWMineSweeper/ASWMS_Engine.cpp
+++ Source/ASWMineSweeper/ASWMS_Engine.cpp
@@ -23,6 +23,8 @@ limitations under the License.
 #include "ASWMS_Engine.h"
 //---------------------------------------------------------------------------
 #include <algorithm>
+#include <stdlib.h>
+#include <time.h>
 //---------------------------------------------------------------------------
 #include "ASWMS_Cell.h"
 //---------------------------------------------------------------------------
@@ -37,7 +39,12 @@ namespace ASWMS
 //---------------------------------------------------------------------------
 TMSEngine::TMSEngine()
     : m_firstClick(true),
+      m_GameState(EGameState::NotSet),
+      m_MouseDown_Shift(0),
+      m_MouseDown_X(-1),
+      m_MouseDown_Y(-1),
       m_NumMines(0),
+      m_StartTick(StartTick_NotSet),
       Grid(nullptr)
 {
 }
@@ -47,60 +54,135 @@ TMSEngine::~TMSEngine()
     delete Grid;
 }
 //---------------------------------------------------------------------------
-void TMSEngine::DrawCell(TImage* image, size_t row, size_t col, int xPos, int yPos, int mouseX, int mouseY)
+void TMSEngine::DoClick(TShiftState shift, size_t row, size_t col)
 {
-    TCell const* cell = &(*Grid->Rows)[row][col];
+    size_t nCols = Grid->GetColCount();
+    size_t nRows = Grid->GetRowCount();
+
+    if (row >= nRows || col >= nCols)
+        return;
+
+    TCell* cell = Grid->GetCell(row, col);
+
+    if (shift.Contains(ssLeft) && shift.Contains(ssRight))
+    {
+
+    }
+    else if (shift.Contains(ssLeft))
+    {
+        if (cell->MarkedAsQuestion || cell->MarkedAsMine)
+        {
+            // Do nothing - don't allow a click to reveal a cell when the user has a marked it sus
+        }
+        else if (cell->IsMine)
+        {
+            m_GameState = EGameState::GameOver_Boom;
+            RevealAll();
+        }
+        else if (!cell->Discovered)
+        {
+            cell->Discovered = true;
+            int nMines = GetNeighboringMineCount(row, col);
+            if (0 == nMines)
+            {
+                // Start top left then go clockwise around this cell
+                DoClick(shift, row - 1, col - 1);
+                DoClick(shift, row - 1, col);
+                DoClick(shift, row - 1, col + 1);
+                DoClick(shift, row, col - 1);
+                DoClick(shift, row, col + 1);
+                DoClick(shift, row + 1, col - 1);
+                DoClick(shift, row + 1, col);
+                DoClick(shift, row + 1, col + 1);
+            }
+        }
+    }
+    else if (shift.Contains(ssRight))
+    {
+        if (cell->MarkedAsMine)
+        {
+            cell->MarkedAsMine = false;
+            cell->MarkedAsQuestion = true;
+        }
+        else if (cell->MarkedAsQuestion)
+        {
+            cell->MarkedAsQuestion = false;
+        }
+        else
+        {
+            cell->MarkedAsMine = true;
+        }
+    }
+}
+//---------------------------------------------------------------------------
+void TMSEngine::DrawCell(
+    TImage* image, size_t row, size_t col, int xPos, int yPos, TShiftState shift, int mouseX, int mouseY)
+{
+    TCell const* cell = Grid->GetCell(row, col);
     Graphics::TBitmap* bmp = image->Picture->Bitmap;
     TCanvas* canvas = bmp->Canvas;
-    Graphics::TBitmap* bmpCell = nullptr;
+    Graphics::TBitmap* bmpBack = Sprites.Tiles[static_cast<size_t>(ETile::Covered)].Bmp;
+    Graphics::TBitmap* bmpFore = nullptr;
 
     if (cell->Discovered)
     {
+        bmpBack = Sprites.Tiles[static_cast<size_t>(ETile::Uncovered)].Bmp;
+
         if (cell->IsMine)
-            bmpCell = Sprites.Mine.Bmp;
+        {
+            bmpFore = Sprites.Mine.Bmp;
+        }
         else
-            bmpCell = Sprites.Tiles[static_cast<size_t>(ETile::Uncovered)].Bmp;
+        {
+            int nMines = GetNeighboringMineCount(row, col);
+            if (nMines > 0)
+                bmpFore = Sprites.Digits_Proximity[static_cast<size_t>(nMines - 1)].Bmp;
+        }
     }
     else if (cell->MarkedAsMine)
     {
-        bmpCell = Sprites.Flag.Bmp;
+        bmpFore = Sprites.Flag.Bmp;
     }
     else if (cell->MarkedAsQuestion)
     {
-        bmpCell = Sprites.Question.Bmp;
+        bmpFore = Sprites.Question.Bmp;
     }
-    else
-    {
-        bmpCell = Sprites.Tiles[static_cast<size_t>(ETile::Covered)].Bmp;
 
+    if (!cell->Discovered)
+    {
         // Is the mouse over the cell
-        if (mouseX >= xPos && mouseX < xPos + bmpCell->Width &&
-            mouseY >= yPos && mouseY < yPos + bmpCell->Height)
+        if (mouseX >= xPos && mouseX < xPos + bmpBack->Width &&
+            mouseY >= yPos && mouseY < yPos + bmpBack->Height)
         {
-            bmpCell = Sprites.Tiles[static_cast<size_t>(ETile::CoveredLit)].Bmp;
+            bmpBack = Sprites.Tiles[static_cast<size_t>(ETile::CoveredLit)].Bmp;
+
+            if (shift.Contains(ssLeft))
+            {
+                bmpBack = Sprites.Tiles[static_cast<size_t>(ETile::CoveredClicked)].Bmp;
+            }
         }
     }
 
-    canvas->Draw(xPos, yPos, bmpCell);
+    canvas->Draw(xPos, yPos, bmpBack);
 
-    TRect rect(xPos, yPos, xPos + bmpCell->Width - 1, yPos + bmpCell->Height - 1);
+    if (nullptr != bmpFore)
+    {
+        bmpFore->Transparent = true;
+        canvas->Draw(xPos, yPos, bmpFore);
+    }
+
+    TRect rect(xPos, yPos, xPos + bmpBack->Width - 1, yPos + bmpBack->Height - 1);
     canvas->Brush->Color = TColor(0xFF1C69B3);
     canvas->FrameRect(rect);
 }
 //---------------------------------------------------------------------------
-void TMSEngine::DrawMap(TImage* image, int mouseX, int mouseY)
+void TMSEngine::DrawMap(TImage* image, TShiftState shift, int mouseX, int mouseY)
 {
     int cellWidth = GetCellDrawWidth();
     int cellHeight = GetCellDrawHeight();
-    size_t nRows = Grid->Rows->size();
 
-    if (0 == nRows)
-        return; // Should never happen
-
-    size_t nCols = (*Grid->Rows)[0].size();
-
-    if (0 == nCols)
-        return; // Should never happen
+    size_t nRows = Grid->GetRowCount();
+    size_t nCols = Grid->GetColCount();
 
     for (size_t row = 0; row < nRows; row++)
     {
@@ -109,9 +191,15 @@ void TMSEngine::DrawMap(TImage* image, int mouseX, int mouseY)
         for (size_t col = 0; col < nCols; col++)
         {
             int xOffset = static_cast<int>(col) * cellWidth;
-            DrawCell(image, row, col, xOffset, yOffset, mouseX, mouseY);
+            DrawCell(image, row, col, xOffset, yOffset, shift, mouseX, mouseY);
         }
     }
+}
+//---------------------------------------------------------------------------
+void TMSEngine::DrawMap(TImage* image)
+{
+    TShiftState dummy;
+    DrawMap(image, dummy, -1, -1);
 }
 //---------------------------------------------------------------------------
 int TMSEngine::GetCellDrawHeight()
@@ -126,21 +214,99 @@ int TMSEngine::GetCellDrawWidth()
 //---------------------------------------------------------------------------
 int TMSEngine::GetDrawHeight()
 {
-    if (Grid->Rows->size() == 0)
-        return 0;
-
-    int result = static_cast<int>(Grid->Rows->size()) * Sprites.Tiles[0].Bmp->Height;
-    return result;
+    return static_cast<int>(Grid->GetRowCount()) * Sprites.Tiles[0].Bmp->Height;
 }
 //---------------------------------------------------------------------------
 int TMSEngine::GetDrawWidth()
 {
-    if (Grid->Rows->size() == 0 || Grid->Rows[0].size() == 0)
-        return 0;
+    //if (Grid->GetRowCount() == 0 || Grid->GetColCount() == 0)
+    //    return 0;
 
-    TGrid::TRow const* row = &(*Grid->Rows)[0];
-    int result = static_cast<int>(row->size()) * Sprites.Tiles[0].Bmp->Width;
-    return result;
+    //TGrid::TRow const* row = &(*Grid->Rows)[0];
+    //int result = static_cast<int>(row->size()) * Sprites.Tiles[0].Bmp->Width;
+    //return result;
+    return static_cast<int>(Grid->GetColCount()) * Sprites.Tiles[0].Bmp->Width;
+}
+//---------------------------------------------------------------------------
+EGameState TMSEngine::GetGameState()
+{
+    return m_GameState;
+}
+//---------------------------------------------------------------------------
+int TMSEngine::GetNeighboringMineCount(size_t row, size_t col) const
+{
+    int count = 0;
+    size_t nCols = Grid->GetColCount();
+    size_t nRows = Grid->GetRowCount();
+
+    // Top row
+    if (row > 0 && col > 0 && Grid->GetCell(row - 1, col - 1)->IsMine)
+        count++;
+
+    if (row > 0 && Grid->GetCell(row - 1, col)->IsMine)
+        count++;
+
+    if (row > 0 && col + 1 < nCols && Grid->GetCell(row - 1, col + 1)->IsMine)
+        count++;
+
+    // Left and right
+    if (col > 0 && Grid->GetCell(row, col - 1)->IsMine)
+        count++;
+
+    if (col + 1 < nCols && Grid->GetCell(row, col + 1)->IsMine)
+        count++;
+
+    // bottom row
+    if (row + 1 < nRows && col > 0 && Grid->GetCell(row + 1, col - 1)->IsMine)
+        count++;
+
+    if (row + 1 < nRows && Grid->GetCell(row + 1, col)->IsMine)
+        count++;
+
+    if (row + 1 < nRows && col + 1 < nCols && Grid->GetCell(row + 1, col + 1)->IsMine)
+        count++;
+
+    return count;
+}
+//---------------------------------------------------------------------------
+ULONGLONG TMSEngine::GetStartedTick64() const
+{
+    return m_StartTick;
+}
+//---------------------------------------------------------------------------
+void TMSEngine::GridCoordsFromMouse(size_t* col, size_t* row, int x, int y)
+{
+    if (nullptr != col)
+        *col = static_cast<size_t>(x / GetCellDrawWidth());
+
+    if (nullptr != row)
+        *row = static_cast<size_t>(y / GetCellDrawHeight());
+}
+//---------------------------------------------------------------------------
+void TMSEngine::MouseDown(TShiftState shift, int x, int y)
+{
+    m_MouseDown_Shift = shift;
+    m_MouseDown_X = x;
+    m_MouseDown_Y = y;
+}
+//---------------------------------------------------------------------------
+void TMSEngine::MouseUp(TShiftState shift, int x, int y)
+{
+    if (m_firstClick && !shift.Contains(ssLeft))
+        return;
+
+    size_t row;
+    size_t col;
+    GridCoordsFromMouse(&col, &row, x, y);
+
+    if (m_firstClick)
+    {
+        PopulateMineField(row, col);
+        m_GameState = EGameState::InProgress;
+    }
+
+    DoClick(shift, row, col);
+    m_firstClick = false;
 }
 //---------------------------------------------------------------------------
 void TMSEngine::NewGame(size_t nRows, size_t nCols, int nMines, TImage* image)
@@ -149,6 +315,8 @@ void TMSEngine::NewGame(size_t nRows, size_t nCols, int nMines, TImage* image)
     Grid = new TGrid(nRows, nCols);
 
     m_firstClick = true;
+    m_StartTick = StartTick_NotSet;
+    m_GameState = EGameState::NewGame;
     m_NumMines = std::min(static_cast<int>(nRows * nCols) - 1, nMines);
 
     Graphics::TBitmap* bmp = image->Picture->Bitmap;
@@ -157,6 +325,73 @@ void TMSEngine::NewGame(size_t nRows, size_t nCols, int nMines, TImage* image)
     canvas->Font->Color = clWhite;
     bmp->Width = GetDrawWidth();
     bmp->Height = GetDrawHeight();
+}
+//---------------------------------------------------------------------------
+void TMSEngine::PopulateMineField(size_t mouseRow, size_t mouseCol)
+{
+    static const int maxWhileLoopCounts = 100;
+
+    int mineCount = 0;
+    int mineWhileLoopCount = 0;
+
+    size_t nRows = Grid->GetRowCount();
+    size_t nCols = Grid->GetColCount();
+
+    int totalCells = static_cast<int>(nRows * nCols);
+    if (0 == totalCells)
+        return; // Should never happen
+
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    // Defend against an infinite loop
+    if (m_NumMines >= totalCells)
+        m_NumMines = totalCells - 1;
+
+    // Try to get the best population of mines the first time through the field
+    int chance = static_cast<int>(static_cast<double>(m_NumMines) / static_cast<double>(totalCells) * 100.0);
+    if (chance < 8)
+        chance = 8;
+
+    do
+    {
+        for (size_t row = 0; row < nRows && mineCount < m_NumMines; row++)
+        {
+            for (size_t col = 0; col < nCols && mineCount < m_NumMines; col++)
+            {
+                // Don't populate a mine where the click occurred - give user a break on the first click
+                if (row == mouseRow && col == mouseCol)
+                    continue;
+
+                bool setMine = ((std::rand() % 100) < chance);
+                if (!setMine && m_NumMines < totalCells - 1)
+                    continue;
+
+                mineCount++;
+                TCell* cell = Grid->GetCell(row, col);
+                cell->IsMine = true;
+            }
+        }
+
+        mineWhileLoopCount++;
+    } while (mineCount < m_NumMines && mineWhileLoopCount < maxWhileLoopCounts);
+
+    if (mineCount < m_NumMines)
+        m_NumMines = mineCount; // Unexpected unless the mine count to total cells ratio is too high (like 999/1000)
+}
+//---------------------------------------------------------------------------
+void TMSEngine::RevealAll()
+{
+    size_t nRows = Grid->GetRowCount();
+    size_t nCols = Grid->GetColCount();
+
+    for (size_t row = 0; row < nRows; row++)
+    {
+        for (size_t col = 0; col < nCols; col++)
+        {
+            TCell* cell = Grid->GetCell(row, col);
+            cell->Discovered = true;
+        }
+    }
 }
 //---------------------------------------------------------------------------
 
--- Source/ASWMineSweeper/ASWMS_Engine.h
+++ Source/ASWMineSweeper/ASWMS_Engine.h
@@ -21,8 +21,12 @@ limitations under the License.
 #ifndef ASWMS_EngineH
 #define ASWMS_EngineH
 //---------------------------------------------------------------------------
+#include <windows.h>
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
 #include <Vcl.ExtCtrls.hpp>
 //---------------------------------------------------------------------------
+#include "ASWMS_Cell.h"
 #include "ASWMS_Grid.h"
 #include "ASWMS_Sprites.h"
 //---------------------------------------------------------------------------
@@ -30,11 +34,24 @@ limitations under the License.
 namespace ASWMS
 {
 
+enum class EGameState
+{
+    NotSet,
+    NewGame,
+    InProgress,
+    GameOver_Boom,
+    GameOver_Win,
+};
+
+
 /////////////////////////////////////////////////////////////////////////////
 // TMSEngine
 /////////////////////////////////////////////////////////////////////////////
 class TMSEngine
 {
+private: // Static vars
+    static ULONGLONG const StartTick_NotSet = static_cast<ULONGLONG>(-1);
+
 public: // Static vars
     static size_t const BeginnerRows = 8;
     static size_t const BeginnerCols = 8;
@@ -48,24 +65,42 @@ public: // Static vars
 
 private:
     bool m_firstClick;
+    EGameState m_GameState;
+    TShiftState m_MouseDown_Shift;
+    int m_MouseDown_X;
+    int m_MouseDown_Y;
     int m_NumMines;
+    ULONGLONG m_StartTick;
 
 public:
     TGrid* Grid;
     TSprites Sprites;
 
 private:
-    void DrawCell(TImage* image, size_t row, size_t col, int xPos, int yPos, int mouseX, int mouseY);
+    void DoClick(TShiftState shift, size_t row, size_t col);
+    void DrawCell(TImage* image, size_t row, size_t col, int xPos, int yPos, TShiftState shift, int mouseX, int mouseY);
+    TCell* GetCell(size_t row, size_t col);
     int GetCellDrawHeight();
     int GetCellDrawWidth();
     int GetDrawHeight();
     int GetDrawWidth();
+    int GetNeighboringMineCount(size_t row, size_t col) const;
+    void GridCoordsFromMouse(size_t* col, size_t* row, int x, int y);
+    void PopulateMineField(size_t mouseRow, size_t mouseCol);
+    void RevealAll();
+
+public:
+    EGameState GetGameState();
+    ULONGLONG GetStartedTick64() const;
 
 public:
     TMSEngine();
     ~TMSEngine();
 
-    void DrawMap(TImage* image, int mouseX, int mouseY);
+    void DrawMap(TImage* image, TShiftState shift, int mouseX, int mouseY);
+    void DrawMap(TImage* image);
+    void MouseDown(TShiftState shift, int x, int y);
+    void MouseUp(TShiftState shift, int x, int y);
     void NewGame(size_t nRows, size_t nCols, int nMines, TImage* image);
 };
 
